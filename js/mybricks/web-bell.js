d29b09c364b40af238413d40e4597a852ace9334
Soledad Penades <sole@mozilla.com>
2013-09-25 23:53:52 +0100

    More

--- js/mybricks/web-bell.js
+++ js/mybricks/web-bell.js
@@ -11,31 +11,13 @@
             'tap': function(e) {
                 e.preventDefault();
                 e.stopPropagation();
-                this.audioTag.currentTime = 0;
-                this.audioTag.play();
+                this.dingDong();
             }
         },
         methods: {
-            noteOn: function(frequency) {
-                this.audioNode.frequency.value = frequency;
-                console.log('note On TODO:', frequency, this.audioNode);
-                this.audioNode.start(0);
-                //this.audioNode.start(this.audioNode.context.currentTime);
-            },
-            noteOff: function() {
-                var ctx = this.audioNode.context;
-
-                this.audioNode.stop(0);
-                this.audioNode.disconnect();
-
-                this.audioNode = ctx.createOscillator();
-                
-                var parentAudioNode = this.parentNode && this.parentNode.audioNode;
-                console.log(parentAudioNode);
-                if(parentAudioNode) {
-                    this.audioNode.connect(parentAudioNode);
-                }
-                //this.audioNode.stop(this.audioNode.context.currentTime);
+            dingDong: function() {
+                this.audioTag.currentTime = 0;
+                this.audioTag.play();
             }
         }
     });
