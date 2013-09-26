7035d60cb51fb52b9d207f32496ccae3c44c8e99
Soledad Penades <sole@mozilla.com>
2013-09-26 11:29:14 +0100

    stopPropagation in slide links

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
 window.addEventListener('DOMComponentsLoaded', function() {
     Brickpresso.init();
+
+    // ensures links in the pres don't trigger slide changes. Might want to move it to brickpresso code?
+    arr(document.querySelectorAll('a')).forEach(function(a) {
+        a.target = "_blank";
+        a.addEventListener('click', function(e) {
+            e.stopPropagation();
+        }, false);
+    });
+
+    function arr(list) {
+        return Array.prototype.slice.call(list);
+    }
 }, false);
 
